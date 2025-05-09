fafd91d72d6fee8290c3b4c321961f45ecd764af
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-09 17:30:05 +0200

    Add section 4

---
+++ section4/springsection4/src/main/java/com/moldavets/eazy_bank/config/EazyBankUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.moldavets.eazy_bank.config;
+
+import com.moldavets.eazy_bank.model.Customer;
+import com.moldavets.eazy_bank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EazyBankUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public EazyBankUserDetailsService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        return new User(customer.getEmail(), customer.getPassword(), authorities);
+    }
+}
