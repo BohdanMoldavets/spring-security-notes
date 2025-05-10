b0fc83036e1fa2420cdb9d52cbe3d3b6c2cd99b0
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-05-10 17:39:04 +0200

    Add section 4

--- section4/springsection4/src/main/java/com/moldavets/eazy_bank/config/ProjectSecurityConfig.java
+++ section4/springsection4/src/main/java/com/moldavets/eazy_bank/config/ProjectSecurityConfig.java
@@ -2,22 +2,23 @@ package com.moldavets.eazy_bank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/loans","/accounts", "/cards", "/balances").authenticated()
-                .requestMatchers("/notices", "/contacts", "/error").permitAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/loans","/accounts", "/cards", "/balances").authenticated()
+                    .requestMatchers("/notices", "/contacts", "/error",  "/register").permitAll());
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
@@ -33,9 +34,9 @@ public class ProjectSecurityConfig {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Bean
-    public CompromisedPasswordChecker compromisedPasswordChecker() {
-        return new HaveIBeenPwnedRestApiPasswordChecker();
-    }
+//    @Bean
+//    public CompromisedPasswordChecker compromisedPasswordChecker() {
+//        return new HaveIBeenPwnedRestApiPasswordChecker();
+//    }
 
 }
